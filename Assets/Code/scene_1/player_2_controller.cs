bf05f64f07f3a846893e2d950c5da51bc05fa91d
Zichu08 <143557155+Zichu08@users.noreply.github.com>
2024-02-13 03:37:03 -0600

    0.1
    - added two characters
    - created a basic map
    - jump, double jump + keyboard movement

---
+++ Assets/Code/scene_1/player_2_controller.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace scene_1 {
+    public class player_2_controller : MonoBehaviour {
+        // Outlet
+        Rigidbody2D _rigidbody2D;
+
+        // State Tracking
+        public int jumpsLeft;
+
+        // Methods
+        void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Move Player Left 
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                _rigidbody2D.AddForce(Vector2.left * 18f * Time.deltaTime, ForceMode2D.Impulse);
+            }
+
+            // Move Player Right
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                _rigidbody2D.AddForce(Vector2.right * 18f * Time.deltaTime, ForceMode2D.Impulse);
+            }
+            
+            // Jump
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (jumpsLeft > 0)
+                {
+                    jumpsLeft--;
+                    _rigidbody2D.AddForce(Vector2.up * 8f, ForceMode2D.Impulse);
+                }
+            }
+        }
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            // Check that we collided with Ground
+            if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            {
+                // Check what is directly below our character's feet
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.down, 0.7f);
+                // Debug.DrawRay(transform.position, Vector2.down * 0.7f); // Visualize Raycast
+
+                // We might have multiple things below out character's feet
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    RaycastHit2D hit = hits[i];
+
+                    // Check that we collided with ground below our feet
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                    {
+                        // Reset jump count
+                        jumpsLeft = 2;
+                    }
+                }
+            }
+        }
+    }
+}
