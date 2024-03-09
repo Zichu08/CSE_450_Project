0caeaa4b6dabbd5f2f3849067bb77be5169675cc
Vraj Patel <p.vraj@wustl.edu>
2024-03-08 23:42:06 -0600

    finished boundary and it works!

--- Assets/Code/scene_1/Boundary.cs
+++ Assets/Code/scene_1/Boundary.cs
@@ -8,15 +8,40 @@ namespace scene_1
     public class Boundary : MonoBehaviour
     {
         
+        [SerializeField] private player_1_controller player1Controller;
+        [SerializeField] private player_2_controller player2Controller;
+        [SerializeField] private GameObject gameOverText;
+
+        [SerializeField] private GameObject p1WinsText;
+        [SerializeField] private GameObject p2WinsText;
+        
+        public GameObject button;
+
+        
         private void OnCollisionEnter2D(Collision2D collision)
         {
             // Check if the collider belongs to the player
             if(collision.gameObject.GetComponent<player_1_controller>())
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-
-                Debug.Log("Player hit the boundary!");
+                Debug.Log("Player 1 hit the boundary!");
+                GameOver();
+                p2WinsText.SetActive(true);
             }
+            
+            if(collision.gameObject.GetComponent<player_2_controller>())
+            {
+                Debug.Log("Player 2 hit the boundary!");
+                GameOver();
+                p1WinsText.SetActive(true);
+            }
+        }
+        
+        private void GameOver()
+        {
+            if (player1Controller != null) player1Controller.DisableMovement();
+            if (player2Controller != null) player2Controller.DisableMovement();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            button.SetActive(true);
         }
     }
 }
