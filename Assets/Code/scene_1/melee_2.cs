ac3efd0f53e22bc2a8ed2592e7383a7e243ccb8d
Zichu08 <143557155+Zichu08@users.noreply.github.com>
2024-03-29 21:00:00 -0500

    melee both

--- Assets/Code/scene_1/melee_2.cs
+++ Assets/Code/scene_1/melee_2.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
-public class melee_1: MonoBehaviour
+public class melee_2: MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator animator;
@@ -14,7 +15,7 @@ public class melee_1: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.N))
         {
             Attack();
         } 
@@ -24,19 +25,18 @@ public class melee_1: MonoBehaviour
     {
         // Play an attack animation
         animator.SetTrigger("Attack");
+        
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayers);
-        // Diebug.Log("running");
         foreach (Collider2D player in hitEnemies)
         {
-             Debug.Log("running");
-            if (player.name == "player_2")
+            if (player.name == "player_1")
             {
-               // Debug.Log(enemy.name);
+                Debug.Log(player.name);
 
-                //Debug.Log("We hit" + enemy.name);
+                Debug.Log("We hit" + player.name);
                 player.GetComponent<health_manager>().RemoveHealth(20);
-            }
 
+            }
         }
         
     }
