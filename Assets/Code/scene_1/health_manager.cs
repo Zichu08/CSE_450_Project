f658ba87b8c91342ec0ad616af3877b521bcea09
bashar43 <89860891+bashar43@users.noreply.github.com>
2024-04-14 15:55:07 -0500

    respawn working -- not including falling off map

--- Assets/Code/scene_1/health_manager.cs
+++ Assets/Code/scene_1/health_manager.cs
@@ -5,98 +5,112 @@ public class health_manager : MonoBehaviour {
     public GameObject heartPrefab;
     public Transform heartsContainer; // Reference to the Canvas transform
 
-        // Data members
-        public int health = 199;
-        public bool alive = true;
-        public health_bar_controller health_bar;
-        public int livesRemaining;
-        public int maxHearts;
+    // Data members
+    public int health = 199;
+    public bool alive = true;
+    public health_bar_controller health_bar;
+    public int livesRemaining;
+    public int maxHearts;
+    private Vector3 startPosition;
 
-        /*
-         * Set health points to a specified number
-         * Parameters - healthPoints: A single unit of health
-         */
-        public void SetHealth(int healthPoints)
+    /*
+        * Set health points to a specified number
+        * Parameters - healthPoints: A single unit of health
+        */
+    public void SetHealth(int healthPoints)
+    {
+        health = healthPoints;
+        health_bar.set_max_health(200);
+    }
+
+    private void Start() {
+
+        livesRemaining = 3;
+        startPosition = this.transform.position;
+        SetHealth(200);
+        RenderHearts();
+        alive = true;
+    }
+
+
+    /*
+        * Add a specified number of health points to a player's health
+        * Parameters - healthPoints: A single unit of health
+        */
+    public void AddHealth(int healthPoints)
+    {
+        int healthSum = health + healthPoints;
+        if (healthSum < 100)
         {
-            health = healthPoints;
-            health_bar.set_max_health(200);
+            health = healthSum;
+        }
+        else
+        {
+            health = 100;
         }
 
-        private void Start() {
-            livesRemaining = 3;
-            SetHealth(200);
-            RenderHearts();
+        // If for some reason the player is gaining health after being dead, alive is reset as true
+        if (!alive)
+        {
             alive = true;
         }
+    }
 
+    /*
+    * Remove a specified number of health points from a player's health
+    * Parameters - healthPoints: A single unit of health
+    */
 
-        /*
-         * Add a specified number of health points to a player's health
-         * Parameters - healthPoints: A single unit of health
-         */
-        public void AddHealth(int healthPoints)
+    public void RemoveHealth(int healthPoints)
+    {
+        if (!alive)
         {
-            int healthSum = health + healthPoints;
-            if (healthSum < 100)
-            {
-                health = healthSum;
-            }
-            else
-            {
-                health = 100;
-            }
-
-            // If for some reason the player is gaining health after being dead, alive is reset as true
-            if (!alive)
-            {
-                alive = true;
-            }
+            return; // If the player is already dead, don't process further
         }
 
-        /*
-         * Remove a specified number of health points from a player's health
-         * Parameters - healthPoints: A single unit of health
-         */
-
-        public void RemoveHealth(int healthPoints)
+        health -= healthPoints;
+        if (health <= 0)
         {
-            if (!alive)
+            if (livesRemaining > 0)
             {
-                return; // If the player is already dead, don't process further
-            }
-
-            health -= healthPoints;
-            if (health <= 0 && livesRemaining > 0)
-            {
-                // Player lost a life but still has more lives remaining
                 livesRemaining--; // Lose a life
                 RenderHearts(); // Render hearts
                 health_bar.set_health(0); // Set the health bar to zero
-
-                if (livesRemaining > 0)
-                {
-                    // Reset health for the next life
-                    SetHealth(200);
-                }
-                else
-                {
-                    // No more lives left, the player is now dead
-                    alive = false;
-                }
+                Respawn(); // Reset position and health
             }
             else
             {
-                // Update the health bar if health is still above 0
-                health_bar.set_health(health);
+                // No more lives left, the player is now dead
+                alive = false;
             }
         }
+        else
+        {
+            // Update the health bar if health is still above 0
+            health_bar.set_health(health);
+        }
+    }
 
+    /*
+        * Respawn function on losing a heart.
+        */
 
+    private void Respawn()
+    {
+        if (livesRemaining > 0)
+        {
+            // Set the player's position back to the start position
+            transform.position = startPosition;
 
-        /*
-         * Return a player's health
-         */
-        public int GetHealth()
+            // Reset health to full
+            SetHealth(200);
+        }
+    }
+
+    /*
+     * Return a player's health
+     */
+    public int GetHealth()
         {
             return health;
         }
