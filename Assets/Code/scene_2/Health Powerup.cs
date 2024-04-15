67dd3c09fce81473fff4e6131d7fefb6e9bc7651
Connor Irwin <c.p.irwin@wustl.edu>
2024-04-15 16:52:05 -0500

    Forest scene

--- Assets/Code/scene_2/Health Powerup.cs
+++ Assets/Code/scene_2/Health Powerup.cs
@@ -8,16 +8,20 @@ namespace scene_2
     {
         public static float secondsActive = 7.5f;
 
+        private float startXPos;
+        private float startYPos;
+
         // Start is called before the first frame update
         void Start()
         {
-
+            startXPos = transform.position.x;
+            startYPos = transform.position.y;
         }
 
         // Update is called once per frame
         void Update()
         {
-            transform.position = new Vector2(18.02f, 1f + Mathf.Sin(Time.realtimeSinceStartup) * 0.25f);
+            transform.position = new Vector2(startXPos, startYPos + Mathf.Sin(Time.realtimeSinceStartup) * 0.25f);
         }
 
         public float GetSecondsActive()
